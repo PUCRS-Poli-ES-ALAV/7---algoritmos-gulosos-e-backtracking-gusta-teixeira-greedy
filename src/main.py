a34503dd65007c7fa94416447a6d7440c6bf4620
gustateixeira <gustavohteixeira2904@gmail.com>
2025-04-30 22:10:46 -0300

    1 e 2 feitos

---
+++ src/main.py
@@ -0,0 +1,36 @@
+def getTroco(valor:int, moedasDisp:list[int]) -> dict:
+    resp = dict()
+    moedasDisp.sort()
+    moedasDisp.reverse()
+    for moeda in moedasDisp:
+        aux  = int( valor / moeda)
+        valor = valor % moeda
+        resp[moeda] = aux
+
+    return resp
+
+def sdm_guloso(s:list[int], f:list[int]):
+    x = []
+    i = 0
+
+    for k in range(1,len(f)):
+        print("k: ", k)
+        print("i: ", i)
+
+        if s[k] > f[i]:
+            x.append(k)
+            i = k
+    return x
+
+
+
+
+
+s = [4,6,13,4,2,6,7,9,1,3,9]
+s.sort()
+f = [8, 7, 14, 5, 4,9,10,11,6,13,12]
+f.sort()
+
+print(sdm_guloso(s, f))
+
+print(getTroco(289, [1,5,10,25,100]))
